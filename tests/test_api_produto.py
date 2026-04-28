1fe0d0f3ef928e072ebe75a0acdda8a6c0eea298
Felipe <felipegonc2020@gmail.com>
2026-04-27 23:21:12 -0300

    teste de api-produto

---
+++ tests/test_api_produto.py
@@ -0,0 +1,17 @@
+
+def post_get_e_put_produto(client):
+    response = client.post("/produtos", json={"codigo":1, "valor":30.0, "tipo":2, "desconto_percentual": 0.0})
+    assert response.status_code == 200
+    assert response.json()["codigo"]==1
+
+    response2 = client.get("/produtos/1")
+    assert response2.status_code == 200
+    assert response2.json()["valor"]==30.0
+
+    response3 = client.put("/produtos/1/40.5")
+    assert response3.status_code == 200
+    assert response3.json()["valor"]==40.5
+
+def test_get_produto_inexistente(client):
+    response = client.get("/produtos/000")
+    assert response.status_code == 404
